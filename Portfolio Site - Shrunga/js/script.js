77a63f907e8b607bb5fbc0a01ff0c56da4b0a516
Shrunha13 <69629093+Shrunha13@users.noreply.github.com>
2020-08-23 14:20:33 -0400

    updated for 2.1

--- Portfolio Site - Shrunga/js/script.js
+++ Portfolio Site - Shrunga/js/script.js
@@ -1,1 +1,9 @@
-Console.log = (Hello World!);
+console.log("Hey! Welcome to My Portfolio Site");
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
