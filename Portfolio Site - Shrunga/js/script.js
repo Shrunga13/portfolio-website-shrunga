879752081d64886a9728f7e5959bd42f983c797c
Shrunha13 <69629093+Shrunha13@users.noreply.github.com>
2020-08-28 18:49:34 -0400

    update for 2.5

--- Portfolio Site - Shrunga/js/script.js
+++ Portfolio Site - Shrunga/js/script.js
@@ -1,9 +1,20 @@
 console.log("Hey! Welcome to My Portfolio Site");
+
 function menuToggle() {
-    var x = document.getElementById("myNavtoggle");
-    if (x.className === "navtoggle") {
-        x.className += " responsive";
-    } else {
-        x.className = "navtoggle";
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
+
+function.menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+
 }
